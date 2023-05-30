483643f387019f66ca3ca48a2bb03209318ce4e5
Santhu2022 <mogilipakasantosh@gmail.com>
2023-05-30 13:09:49 +0000

    Full Code

---
+++ src/components/Layout/index.js
@@ -0,0 +1,16 @@
+import Header from '../Header'
+import Body from '../Body'
+import Footer from '../Footer'
+import './index.css'
+
+const Layout = () => (
+  <div className="layout-section">
+    <div>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  </div>
+)
+
+export default Layout
